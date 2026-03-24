d0eaf9697b0d622caf3093f329e39799e6fd85d1
Wntrfl <s35079@pjwstk.edu.pl>
2026-03-24 11:03:37 +0100

    Created Customer class and tested in main

--- src/CafeCustomer.java
+++ src/CafeCustomer.java
@@ -1,2 +1,36 @@
 public class CafeCustomer {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public CafeCustomer(int customerId, String firstName, String lastName, String email) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public void printCustomerInfo() {
+        System.out.println("Customer ID: " + customerId);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Email: " + email);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer ID: %s | First name: %s | Last name: %s |Email:%s |" , customerId, firstName,
+                lastName, email);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CafeCustomer that = (CafeCustomer) o;
+        return customerId == that.customerId&&email.equals(that.email);
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public static void main(String[] args) {
 
+   CafeCustomer customer1 = new CafeCustomer(12345, "Iavn", "Halski", "Ivan@mail");
+
+   customer1.printCustomerInfo();
+
+
 
 
 }
