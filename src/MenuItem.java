7bdd62aff01770032794e8dabfe464fa7d7a484b
Wntrfl <s35079@pjwstk.edu.pl>
2026-03-24 10:47:34 +0100

    Initial commit

---
+++ src/MenuItem.java
@@ -0,0 +1,55 @@
+public class MenuItem {
+    private String code;
+    private String name;
+    private double price;
+    private String category;
+    private static int productCount;
+
+    public MenuItem(String code, String name, double price, String category, int productCount) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.productCount = productCount;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    static int numberOfProducts() {
+        return MenuItem.productCount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Code: %s | Name: %s | Price: $%.2f | Cat: %s",code, price,
+                category, productCount);
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        MenuItem menuItem = (MenuItem) o;
+        return code.equals(menuItem.code);
+    }
+
+
+
+}
